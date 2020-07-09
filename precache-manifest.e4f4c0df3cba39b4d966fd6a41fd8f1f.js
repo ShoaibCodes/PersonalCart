af1ac9a5522a76cae86c354cb63da81d91c4c1b2
Mohammed Shoaib <shoaib.codes@gmail.com>
2020-07-09 21:22:49 +0530

    Updates

---
+++ precache-manifest.e4f4c0df3cba39b4d966fd6a41fd8f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f060cd66d2cc36ef4e3b92d31eebe3",
+    "url": "/PersonalCart/index.html"
+  },
+  {
+    "revision": "afa6699d94fe932dba4f",
+    "url": "/PersonalCart/static/css/main.d2b20dfa.chunk.css"
+  },
+  {
+    "revision": "fa37f865dbfa8a53eb95",
+    "url": "/PersonalCart/static/js/2.f1b28e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PersonalCart/static/js/2.f1b28e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa6699d94fe932dba4f",
+    "url": "/PersonalCart/static/js/main.8e5c13f1.chunk.js"
+  },
+  {
+    "revision": "e942eef538419edcf53f",
+    "url": "/PersonalCart/static/js/runtime-main.7becedb3.js"
+  }
+]);
